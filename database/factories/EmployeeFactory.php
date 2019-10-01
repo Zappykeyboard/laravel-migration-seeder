e321c6a707f281c8d38c938dd2dbf6be799e97e4
Edoardo <a@Air-di-edo.lan>
2019-10-01 16:59:22 +0200

    aggiunti dati fittizi al db

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Employee;
+
+$factory->define(Employee::class, function (Faker $faker) {
+    return [
+      'firstname' => $faker-> firstName,
+      'lastname' => $faker-> lastName,
+      'birthDay' => $faker -> date($format = 'Y-m-d', $max = 'now'),
+      'jobTitle' => $faker-> jobTitle,
+      'salary' => rand(1000, 4000)
+    ];
+});

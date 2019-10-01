86962ad910afd5dedc0c44cb4eececadb1a81627
Edoardo <a@Air-di-edo.lan>
2019-10-01 17:05:42 +0200

    creati dati fittizi per companies

---
+++ database/seeds/CompaniesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Company;
+class CompaniesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       factory(Company::class, 20)->create();
+    }
+}
